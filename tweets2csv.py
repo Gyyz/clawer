02e048fed9c497450c80be5c231843520b53130e
CooDL <yuze.gao@outlook.com>
2017-11-07 16:57:29 +0800

    Update tweets2csv.py

--- tweets2csv.py
+++ tweets2csv.py
@@ -7,9 +7,9 @@ import csv
 
 #Twitter API credentials
 consumer_key = "AopfNa0A9edEPEvCoGnvyg"
-consumer_secret = "jKk5Wy6LILNWpalJNIzzdTc345CceoQL9fthEjD9Q"
-access_key = "190671283-6E4IRCesrlc5yqGr8Ha56wbRH0S36nDCsmk0cdOs"
-access_secret = "wzee8XXKlUMhKoIkx91lkp6O3yAFyGeFDewHOgRbalg"
+consumer_secret = "Kk5Wy6LILNWpalJNIzzdTc345CceoQL9fthEjD9Q"
+access_key = ""
+access_secret = ""
 
 
 def get_all_tweets(screen_name):
